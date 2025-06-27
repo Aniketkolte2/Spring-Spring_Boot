e9e68a54728652733066dc3e9bb856ac54b9e52b
Aniket Rajesh Kolte <143741415+Aniketkolte2@users.noreply.github.com>
2025-06-27 11:37:45 +0530

    Add files via upload

---
+++ AIfirst/AIfirst/src/main/java/com/example/AIfirst/AIfirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.AIfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIfirstApplication.class, args);
+	}
+
+}
---
+++ AIfirst/AIfirst/src/test/java/com/example/AIfirst/AIfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
